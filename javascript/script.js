988d82ede7afd5c377c115bae3fb652a68c3e83c
Tuan Pham <tuan.pham1997@yahoo.com>
2022-05-01 17:28:28 -0400

    finished basic movement, freezing blocks, and basic collision detection

--- javascript/script.js
+++ javascript/script.js
@@ -6,17 +6,10 @@ console.log(canvas)
 const rows = 20
 const col = 10
 const boxSize = 20
-canvas.width = col*boxSize
-canvas.height = rows*boxSize
+canvas.width = col * boxSize
+canvas.height = rows * boxSize
 ctx.scale(boxSize, boxSize)
-ctx.fillStyle = 'black'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
-for(let i=0; i< col; i++){
-    if(i % 2 === 0){
-        ctx.fillStyle = "gray"
-        ctx.fillRect(i,0,1,canvas.height)
-    }
-}
+
 // Tetrominoes!!!! After researching games that had grid arrangement like chess or checkers, decided to make Tetris using arrays in matrix format. The tetrominoes are called T,I,J,L,O,Z, and S.
 const T = [
     [0, 1, 0],
@@ -58,13 +51,116 @@ const I = [
 // Therefore, tetris logic dictates it's 
 // ctx.fillRect(row, column, 1, 1). ctx.scale takes care of the scaling way up there.
 
-function drawPiece(piece){
+function drawPiece(piece, move) {
     piece.forEach((row, y) => {
         row.forEach((val, x) => {
-            if(val !== 0){
+            if (val !== 0) {
                 ctx.fillStyle = 'pink'
-                ctx.fillRect(x + 3, y, 1, 1)
-            }})
+                ctx.fillRect(x + move.x, y + move.y, 1, 1)
+            }
+        })
     })
 }
-drawPiece(I)
+// I've decided to make another array to save my values to simulate tetrominoes freeing in place. The array will be a matrix like the tetrominoes but in 10x20 dimensions. Reminder :increasing y value increases the row count while x increases column count.
+const board = []
+// This for loop is to create my board. Using similiar accessing logic as my drawPiece function to make my board matrix. Looked up how to make an array, using new Array() method
+for (let y = 0; y < rows; y++) {
+    // further research made the for x loop obsolete. Array() takes in desired length as the argument.
+    board.push(new Array(col))
+    board[y].fill(0)
+    // for(let x = 0; x < col; x++){
+    //     board[y][x] = 0
+    // }
+}
+// console.log(board)
+const currentPiece = {
+    x: 4,
+    y: 0,
+    shape: T,
+}
+function drawBoard(board){
+    board.forEach((row, y) => {
+        row.forEach((val, x) =>{
+            if(val !== 0){
+                ctx.fillStyle = 'pink'
+                ctx.fillRect(x, y, 1, 1)
+            }
+        })
+    })
+}
+function drawGame() {
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+    for (let i = 0; i < col; i++) {
+        if (i % 2 === 0) {
+            ctx.fillStyle = "gray"
+            ctx.fillRect(i, 0, 1, canvas.height)
+        }
+    }
+    drawBoard(board)
+    drawPiece(currentPiece.shape, currentPiece)
+}
+let timeNow = 0
+let lastTime = 0
+let timeDiff = 0
+
+function dropPiece(time) {
+    currentPiece.y++
+    console.log(checkOccupied(board,currentPiece))
+    if(checkOccupied(board,currentPiece)){
+        currentPiece.y--
+        freeze(board,currentPiece)
+        currentPiece.y = 0
+        currentPiece.x = 4
+    }
+    return lastTime = time
+}
+
+function update(time) {
+    // let lastTime = 0 If I did this, the requestAnimationFrame function will continuously set my variable to 0. Put it outside.
+    timeNow = time
+    timeDiff = timeNow - lastTime
+    // console.log(timeDiff)
+    if (timeDiff > 1000) {
+        dropPiece(timeNow)
+    }
+    drawGame()
+    requestAnimationFrame(update)
+}
+update()
+// using logic established in drawPiece to access currentPiece and copy it to my game board
+function freeze(board, piece) {
+    piece.shape.forEach((row, y) => {
+        row.forEach((val, x) => {
+            if(val !== 0)
+            board[y + currentPiece.y][x + currentPiece.x] = val
+        })
+    })
+}
+function checkOccupied(board, piece) {
+    for (let y = 0; y < piece.shape.length; y++){
+        for (let x = 0; x < piece.shape[y].length; x++){
+            if (piece.shape[y][x] !== 0 &&
+                (board[y + piece.y] && board[y + piece.y][x + piece.x]) !== 0){
+                    return true
+                }
+        }
+    }
+    return false
+}
+function move(direction) {
+    currentPiece.x += direction
+}
+function movePiece(event) {
+    // console.log(event) Consoled to check event properties to use for movement. Decided on keyCode
+    // keyCodes are 37 : leftArrow, 39 : rightArrow, 40 : downArrow
+    if (event.keyCode === 37) {
+        move(-1)
+    } else if (event.keyCode === 39) {
+        move(1)
+    } else if (event.keyCode === 40) {
+        dropPiece(timeNow)
+        timeDiff = 0
+    }
+}
+document.addEventListener('keydown', movePiece)
--- pseudocode.md
+++ pseudocode.md
@@ -2,11 +2,14 @@ Foundations first
 
 List of needed things for Tetris:
 1. Setup the 7 tetrominoes to interact with along with initial board state
-Lots of arrays are required. Looking into other grid-like such as chess could give me an idea on how to draw tetris.
+--Lots of arrays are required. Looking into other grid-like such as chess could give me an idea on how to draw tetris.
 2. Figure out the clockwise or counter-clockwise rotation of standard Tetris
 3. How to advance game state: lower the selected tetromino 1 line after a brief interval
+-- Based on the video, will use requestAnimationFrame. Seems that it auto plays the function if you self callback. Useful to setup a time.
 4. Figure out how to register collision with other pieces and the game walls
+-- Decided to follow the same logic I've been using and treat Tetris's grid as either "filled" or "not filled" based on the array's value (either 1 or 0).
 5. Keep tetromino placement and get the next piece
+-- Using new Array method, convert my whole playing board as an array to match the data type of my tetrominoes. Using some array logic, I could save these values from one array to another. 
 6. Register and clear completed rows
 
 To-do:
